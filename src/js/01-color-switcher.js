48583fb6b0379085b9dc0dc7ba4a5eeeb372463d
Piotr Chleborowicz <p.chleborowicz@gmail.com>
2024-05-04 15:29:57 +0200

    start i stop tła strony

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-console.log('test');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timeoutId = setTimeout(changeBackgroundColor, 1000);
+}
+
+document
+  .querySelector('button[data-start]')
+  .addEventListener('click', function () {
+    this.disabled = true;
+    document.querySelector('button[data-stop]').disabled = false;
+    changeBackgroundColor();
+  });
+
+document
+  .querySelector('button[data-stop]')
+  .addEventListener('click', function () {
+    this.disabled = true;
+    document.querySelector('button[data-start]').disabled = false;
+    clearTimeout(timeoutId);
+  });
