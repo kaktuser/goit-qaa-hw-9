7922e6f3cd3adac32e832f3a3165d7de2fb622ef
kaktuser <31393824+kaktuser@users.noreply.github.com>
2024-05-04 13:34:48 +0000

    Deploying to gh-pages from @ kaktuser/goit-qaa-hw-9@ac12dcc82fbac3752932d614d5451ad1d176b597 🚀

---
+++ 01-color-switcher.b2177517.js
@@ -0,0 +1,2 @@
+function t(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`;setTimeout(t,1e3)}document.querySelector("button[data-start]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("button[data-stop]").disabled=!1,t()})),document.querySelector("button[data-stop]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("button[data-start]").disabled=!1,clearTimeout(timeoutId)}));
+//# sourceMappingURL=01-color-switcher.b2177517.js.map
